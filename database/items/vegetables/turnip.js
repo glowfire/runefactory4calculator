98e7bcef4777b8a0400334d768d2ce9825b8cc60
glowfire <kunchyi1@gmail.com>
2016-06-27 14:27:44 +0800

    Create turnip.js

---
+++ database/items/vegetables/turnip.js
@@ -0,0 +1,14 @@
+registerItem({
+  "name":"Turnip"
+  "category":"Vegetable"
+  "sell":30
+  "buy":200
+  "effects": {
+    "hp":30
+    "rp":5
+  }
+  "upgradeinfo": {
+    "difficulty":1
+    "str":1
+  }
+})
