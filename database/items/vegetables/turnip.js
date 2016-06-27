12930b1627a7cf543c145dfa54dfd6c659fce10b
glowfire <kunchyi1@gmail.com>
2016-06-27 14:53:09 +0800

    Update turnip.js

--- database/items/vegetables/turnip.js
+++ database/items/vegetables/turnip.js
@@ -1,14 +1,14 @@
 registerItem({
-  "name":"Turnip"
-  "category":"Vegetable"
-  "sell":30
-  "buy":200
+  "name":"Turnip",
+  "category":"Vegetable",
+  "sell":30,
+  "buy":200,
   "effects": {
-    "hp":30
+    "hp":30,
     "rp":5
-  }
+  },
   "upgradeinfo": {
-    "difficulty":1
+    "difficulty":1,
     "str":1
   }
 })
